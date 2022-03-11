ca82201d29973f5a075d97622043293b83af1c9d
Aodan <aodan@aodan.me>
2022-03-11 17:29:21 +0800

    add example

---
+++ Example/Pods/Target Support Files/GJMQTT/GJMQTT-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GJMQTT : NSObject
+@end
+@implementation PodsDummy_GJMQTT
+@end
---
+++ Example/Pods/Target Support Files/Pods-GJMQTT_Example/Pods-GJMQTT_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GJMQTT_Example : NSObject
+@end
+@implementation PodsDummy_Pods_GJMQTT_Example
+@end
